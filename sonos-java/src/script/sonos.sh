23fc1f411237f2661520b507d813582b2dde53c8
Adam Wynne <awynne@gmail.com>
2014-05-25 09:01:59 -0400

    converted to a gradle multi-project build

--- sonos-java/src/script/sonos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-SCRIPT_NAME=$0
-CURRENT_PATH=`dirname "${SCRIPT_NAME}"`
-LIB_PATH="${CURRENT_PATH}/lib/"
-MAIN_JAR=`ls -1 "${LIB_PATH}" 2>/dev/null | grep "sonos-java"`
-java -jar "${LIB_PATH}/${MAIN_JAR}" $*
