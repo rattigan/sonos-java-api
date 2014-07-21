ca55870bf716ecc69ce3c4fcd0430752788b47c5
rattigan <rattigan@gmail.com>
2014-07-21 11:33:14 -0400

    Preliminary work on stripping down to essentials to provide just an API for working with a sonos system. Omitted file

---
+++ src/main/java/org/tensin/sonos/commander/EntryCollector.java
@@ -0,0 +1,36 @@
+package org.tensin.sonos.commander;
+
+import com.beust.jcommander.internal.Lists;
+import org.tensin.sonos.control.BrowseHandle;
+import org.tensin.sonos.control.EntryCallback;
+import org.tensin.sonos.model.Entry;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ */
+public class EntryCollector implements EntryCallback {
+    List<Entry> entries = Lists.newArrayList();
+
+    @Override
+    public void addEntries(BrowseHandle handle, Collection<Entry> entries) {
+        this.entries.addAll(entries);
+    }
+
+    @Override
+    public void retrievalComplete(BrowseHandle handle, boolean completedSuccessfully) {
+        synchronized (handle) {
+            handle.notify();
+        }
+    }
+
+    @Override
+    public void updateCount(BrowseHandle handle, int count) {
+
+    }
+
+    public List<Entry> getEntries() {
+        return entries;
+    }
+}
